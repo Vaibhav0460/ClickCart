ebdd98f7371912aa943f0f8a6494a813910cf78a
Vaibhav Mishra <vaibhav0460@gmail.com>
2025-09-27 11:47:54 +0530

    Bug fix: Edited settings to allow local host

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = '#vw(03o=(9kbvg!&2d5i!2$_58x@_-3l4wujpow6(ym37jxnza'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vaibhav0460.pythonanywhere.com']
+ALLOWED_HOSTS = [
+    'vaibhav0460.pythonanywhere.com',
+    '127.0.0.1',
+    'localhost',]
 
 
 # Application definition
